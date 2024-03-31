ce3395662c5b84ef2c26c8929b5442a98c2239d4
gary <gary313128@gmail.com>
2024-04-01 00:43:16 +0800

    add simple draw

--- b11902090(李冠誼)/b11902090.js
+++ b11902090(李冠誼)/b11902090.js
@@ -12,3 +12,26 @@ function askAge()
 }
 window.addEventListener("DOMContentLoaded",askAge());
 */
+
+function draw()
+{
+    var getRandomInt = (max) => {return Math.floor(Math.random() * max)};
+    var sign = document.getElementById("sign");
+    var num = getRandomInt(6)
+    if(num == 0)
+        sign.innerHTML = "大吉";
+    else if(num == 1)
+        sign.innerHTML = "中吉";
+    else if(num == 2)
+        sign.innerHTML = "小吉";
+    else if(num == 3)
+        sign.innerHTML = "小兇";
+    else if(num == 4)
+        sign.innerHTML = "中兇";
+    else if(num == 5)
+        sign.innerHTML = "大凶";
+}
+
+var drawButton = document.getElementById("draw");
+drawButton.addEventListener("click",draw);
+
