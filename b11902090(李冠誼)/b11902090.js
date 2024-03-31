95b24f09f1a770c70131a562682b536bf10df798
gary <gary313128@gmail.com>
2024-04-01 01:42:35 +0800

    make shrine more small

--- b11902090(李冠誼)/b11902090.js
+++ b11902090(李冠誼)/b11902090.js
@@ -29,14 +29,14 @@ function draw()
             sign.innerHTML = "中吉";
         else if(num == 2)
             sign.innerHTML = "小吉";
+        else if(num == 3)
+            sign.innerHTML = "末吉";
     }
     else 
     {
         sign.style.backgroundColor = "#ff00ff";
-        if(num == 3)
+        if(num == 4)
             sign.innerHTML = "小兇";
-        else if(num == 4)
-            sign.innerHTML = "中兇";
         else if(num == 5)
             sign.innerHTML = "大凶";
     }
