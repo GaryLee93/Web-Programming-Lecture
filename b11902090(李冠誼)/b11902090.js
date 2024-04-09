27e8a030599ff38213236192dce6636be5bbd992
gary <gary313128@gmail.com>
2024-04-09 23:06:18 +0800

    change some typesetting

--- b11902090(李冠誼)/b11902090.js
+++ b11902090(李冠誼)/b11902090.js
@@ -20,28 +20,16 @@ function draw()
     sign.style.borderStyle = "solid";
     sign.style.borderWidth = "medium";
     var num = getRandomInt(6)
-    if(num <= 2) 
-    {
-        sign.style.backgroundColor = "#ff4000";
-        if(num == 0)
-            sign.innerHTML = "大吉";
-        else if(num == 1)
-            sign.innerHTML = "中吉";
-        else if(num == 2)
-            sign.innerHTML = "小吉";
-        else if(num == 3)
-            sign.innerHTML = "末吉";
-    }
-    else 
-    {
-        sign.style.backgroundColor = "#ff00ff";
-        if(num == 4)
-            sign.innerHTML = "小兇";
-        else if(num == 5)
-            sign.innerHTML = "大凶";
-    }
+    if(num == 0)
+        sign.innerHTML = "大吉";
+    else if(num == 1)
+        sign.innerHTML = "中吉";
+    else if(num == 2)
+        sign.innerHTML = "小吉";
+    else if(num == 3)
+        sign.innerHTML = "末吉";
+    else if(num == 4)
+        sign.innerHTML = "小兇";
+    else if(num == 5)
+        sign.innerHTML = "大凶";
 }
-
-var drawButton = document.getElementById("draw");
-drawButton.addEventListener("click",draw);
-
