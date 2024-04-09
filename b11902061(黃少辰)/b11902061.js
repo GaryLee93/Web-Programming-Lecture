db290571950bc9a59382051cb3369e752e00f703
黃少辰 <92316534+FungiSan@users.noreply.github.com>
2024-04-10 03:02:36 +0800

    add timer

--- b11902061(黃少辰)/b11902061.js
+++ b11902061(黃少辰)/b11902061.js
@@ -10,11 +10,34 @@ function withdraw_NASA(){
         for(var i=0; i<nasa_col.length; i++){
             nasa_col[i].innerHTML = "";
         }
-
         alert("恭喜你已停修NASA！");
     }
     else{
         alert("還不快停修！");
     }
+}
+
+var problem2038 = new Date("2038/1/19 3:14:07");
+var nowDate = new Date();
+var timeDiff = problem2038 - nowDate;
+
+window.onload = init;
+
+function init(){
+    countTime();
+    setInterval(countTime, 1000);
+}
+
+function countTime(){
+    var timerElement = document.getElementById("timer");
+    if(timerElement == null)
+        console.warn("timer null");
     
+    timeDiff--;
+    var sec = timeDiff % 60;
+    var min = Math.floor(timeDiff/60) % 60;
+    var hour = Math.floor(timeDiff/3600) % 24;
+    var day = Math.floor(timeDiff/86400);
+
+    timerElement.innerHTML = day + " 天 " + hour + " 小時 " + min + " 分 " + sec + " 秒";
 }
