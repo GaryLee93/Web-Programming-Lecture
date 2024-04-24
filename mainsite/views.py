00867f4fe722764ddf20907be81ffa46b68dec58
change dir structure <gary313128@gmail.com>
2024-04-24 22:26:24 +0800

    solve url problem

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,6 @@
 from django.template.loader import get_template
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from datetime import datetime
 from .models import Post
 
@@ -12,3 +12,13 @@ def homepage(request):
     now = datetime.now()
     html = template.render(locals()) #locals() return all local variabel on memory
     return HttpResponse(html)
+
+def showpost(request,slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
