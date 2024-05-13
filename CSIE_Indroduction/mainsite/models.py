a170145fb366d1ea06a81fb885a024df1e2f6d4a
change dir structure <gary313128@gmail.com>
2024-05-13 22:25:23 +0800

    add news model

--- CSIE_Indroduction/mainsite/models.py
+++ CSIE_Indroduction/mainsite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class News(models.Model):
+    tag = models.CharField(max_length=10)
+    body = models.CharField(max_length=50)
+    uploadTime = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-uploadTime',)
+
+    def __str__(self):
+        return self.title
