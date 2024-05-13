c98eaf2a5acdc14980db3d134c8b56a64db943fc
change dir structure <gary313128@gmail.com>
2024-05-14 01:27:09 +0800

    add prototype of homepage

--- CSIE_Indroduction/mainsite/views.py
+++ CSIE_Indroduction/mainsite/views.py
@@ -1,3 +1,9 @@
+from django.template.loader import get_template
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import News
 # Create your views here.
+
+def homepage(request):
+    allNews = News.objects.all()
+    return render(request,'index.html',locals())
