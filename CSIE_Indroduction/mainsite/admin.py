921fd7f0589fbf6eab0c8c39c54e22661f96e2c7
change dir structure <gary313128@gmail.com>
2024-05-16 18:59:06 +0800

    add function of upload image

--- CSIE_Indroduction/mainsite/admin.py
+++ CSIE_Indroduction/mainsite/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News, Professor
+from mainsite import models
 
 # Register your models here.
 class NewsAdmin(admin.ModelAdmin):
@@ -8,5 +8,9 @@ class NewsAdmin(admin.ModelAdmin):
 class ProfAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'title', 'description')
 
-admin.site.register(News,NewsAdmin)
-admin.site.register(Professor,ProfAdmin)
+class CoruseAdmin(admin.ModelAdmin):
+    list_display=('name','slug','time','credit','description','sweetScale')
+
+admin.site.register(models.News,NewsAdmin)
+admin.site.register(models.Professor,ProfAdmin)
+admin.site.register(models.course,CoruseAdmin)
